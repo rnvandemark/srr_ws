6b35148f25b26ade5b0cd58385247e4a6697d6a1
Nick Vandemark <rnvandemark@gmail.com>
2020-11-14 11:56:58 -0500

    Converted script to use a ROSWrapper class

--- src/srr_teleop/scripts/force_set_positions.py
+++ src/srr_teleop/scripts/force_set_positions.py
@@ -5,7 +5,7 @@ from argparse import ArgumentParser
 from gazebo_msgs.srv import SetModelConfiguration
 
 NODE_NAME = "srr_teleop_force_set_positions"
-SERVICE_TOPIC = "/gazebo/set_model_configuration"
+CLI_SET_MODEL_CONFIGURATION_TOPIC = "/gazebo/set_model_configuration"
 
 APPLICATION_HELP = """
 Use this script to force individual joints of the SRR vehicle model
@@ -18,12 +18,52 @@ Use this script to force individual joints of the SRR vehicle model
  -90 --joint FR_Joint -90 --hz 30\"
 """
 
-def get_rosparam(name):
-    value = None
-    is_set = rospy.has_param(name)
-    if is_set:
-        value = rospy.get_param(name)
-    return is_set, value
+class ROSWrapper(object):
+
+    model_name_is_set           = False
+    model_name                  = None
+    call_rate                   = None
+    joint_names                 = None
+    joint_positions             = None
+    cli_set_model_configuration = None
+
+    def __init__(self, hz, joint_names, joint_positions):
+        rospy.init_node(NODE_NAME)
+
+        self.model_name_is_set, self.model_name = ROSWrapper.get_rosparam("/srr_model_name")
+        self.call_rate                          = None if hz < 1 else rospy.Rate(hz)
+        self.joint_names                        = joint_names
+        self.joint_positions                    = joint_positions
+
+        self.cli_set_model_configuration = rospy.ServiceProxy(CLI_SET_MODEL_CONFIGURATION_TOPIC, SetModelConfiguration)
+
+    def model_is_valid(self):
+        return self.model_name_is_set
+
+    def get_call_rate(self):
+        return self.call_rate
+
+    def has_call_rate(self):
+        return self.get_call_rate() is not None
+
+    def call_cli_set_model_configuration(self):
+        return self.cli_set_model_configuration(
+            model_name=self.model_name,
+            urdf_param_name=self.model_name,
+            joint_names=self.joint_names,
+            joint_positions=self.joint_positions
+        )
+
+    def is_shutdown(self):
+        return rospy.is_shutdown()
+
+    @staticmethod
+    def get_rosparam(name):
+        value = None
+        is_set = rospy.has_param(name)
+        if is_set:
+            value = rospy.get_param(name)
+        return is_set, value
 
 def main():
     parser = ArgumentParser(description=APPLICATION_HELP)
@@ -54,29 +94,27 @@ def main():
         joint_names.append(j[0])
         joint_positions.append(float(j[1]))
 
-    rospy.init_node(NODE_NAME)
-    call_rate = None if args.hz < 1 else rospy.Rate(args.hz)
-    srv_set_model_configuration = rospy.ServiceProxy(SERVICE_TOPIC, SetModelConfiguration)
-    model_name_is_set, model_name = get_rosparam("/srr_model_name")
-    if not model_name_is_set:
+    wrapper = ROSWrapper(args.hz, joint_names, joint_positions)
+
+    if not wrapper.model_is_valid():
         print "Model name is not set in global rosparam server."
         return
 
-    while not rospy.is_shutdown():
-        srv_response = srv_set_model_configuration(
-            model_name=model_name,
-            urdf_param_name=model_name,
-            joint_names=joint_names,
-            joint_positions=joint_positions
-        )
-        if not srv_response.success:
-            print "Exiting, as ROS service call to {0} failed: {1}".format(SERVICE_TOPIC, srv_response.status_message)
-            break
-        elif call_rate is None:
-            print "Task finished successfully. Exiting."
-            break
-        else:
-            call_rate.sleep()
+    try:
+        while not wrapper.is_shutdown():
+            srv_response = wrapper.call_cli_set_model_configuration()
+            if not srv_response.success:
+                print "Exiting, as ROS service call to {0} failed: {1}".format(CLI_SET_MODEL_CONFIGURATION_TOPIC, srv_response.status_message)
+                break
+            elif not wrapper.has_call_rate():
+                print "Task finished successfully. Exiting."
+                break
+            else:
+                wrapper.get_call_rate().sleep()
+    except rospy.service.ServiceException as e:
+        print "Caught a service exception: " + str(e)
+    except rospy.exceptions.ROSInterruptException:
+        print "Exiting."
 
 if __name__ == "__main__":
     main()
