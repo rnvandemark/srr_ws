8941850ee63cc96c0d7f5add59d5b4abdc365b4a
Nick Vandemark <rnvandemark@gmail.com>
2020-11-29 06:47:41 -0500

    Changed bogie pivot nomenclature to leg pivot

--- src/srr_kinematics/src/TractionControlContainer.cpp
+++ src/srr_kinematics/src/TractionControlContainer.cpp
@@ -23,22 +23,22 @@ bool SRR::TractionControlContainer::calculate_vehicle_linear_velocity(tf::Vector
             case MoveTypeEnum::TURN_LEFT:
             case MoveTypeEnum::TURN_RIGHT:
             {
-                BogiePivotEnum bpYof;
-                BogiePivotEnum bpYor;
+                LegPivotEnum lpYof;
+                LegPivotEnum lpYor;
                 if (curr_move_type == MoveTypeEnum::TURN_LEFT)
                 {
-                    bpYof = BogiePivotEnum::RIGHT_NEAR;
-                    bpYor = BogiePivotEnum::RIGHT_FAR;
+                    lpYof = LegPivotEnum::RIGHT_NEAR;
+                    lpYor = LegPivotEnum::RIGHT_FAR;
                 }
                 else
                 {
-                    bpYof = BogiePivotEnum::LEFT_NEAR;
-                    bpYor = BogiePivotEnum::LEFT_FAR;
+                    lpYof = LegPivotEnum::LEFT_NEAR;
+                    lpYor = LegPivotEnum::LEFT_FAR;
                 }
                 double Xfo  = curr_longitudinal_distance_origin_axle;
                 double Xro  = -curr_longitudinal_distance_origin_axle;
-                double dYof = lateral_vector_origin_bogie_pivot.at(bpYof).y() + curr_dist_between_ICC_origin;
-                double dYor = lateral_vector_origin_bogie_pivot.at(bpYor).y() + curr_dist_between_ICC_origin;
+                double dYof = lateral_vector_origin_leg_pivot.at(lpYof).y() + curr_dist_between_ICC_origin;
+                double dYor = lateral_vector_origin_leg_pivot.at(lpYor).y() + curr_dist_between_ICC_origin;
                 double turn_radius_front = std::sqrt((Xfo * Xfo) + (dYof * dYof));
                 double turn_radius_rear  = std::sqrt((Xro * Xro) + (dYor * dYor));
                 x_dot = static_cast<int>(curr_move_direction)
@@ -57,27 +57,28 @@ bool SRR::TractionControlContainer::calculate_vehicle_linear_velocity(tf::Vector
     return rv;
 }
 
-bool SRR::TractionControlContainer::estimate_contact_angles(tf::Vector3 lin_vel, BogieAbstractMap<double>& cont_ang)
+bool SRR::TractionControlContainer::estimate_contact_angles(tf::Vector3 lin_vel, LegAbstractMap<double>& cont_ang)
 {
-    // WARNING: Assumes no rotation from vehicle origin to home position bogie joint, ensure that's correct
-    for (int bp = static_cast<int>(BogiePivotEnum::LEFT_NEAR); static_cast<BogiePivotEnum>(bp) <= BogiePivotEnum::RIGHT_FAR; bp++)
+    // WARNING: Assumes no rotation from vehicle origin to home position leg pivot joint, ensure that's correct
+    int lp_max = static_cast<int>(LegPivotEnum::RIGHT_FAR);
+    for (int lp = static_cast<int>(LegPivotEnum::LEFT_NEAR); lp <= lp_max; lp++)
     {
-        BogiePivotEnum bpe = static_cast<BogiePivotEnum>(bp);
-        tf::Vector3 ang_vel_bogie_frame_wrt_body(
+        LegPivotEnum lpe = static_cast<LegPivotEnum>(lp);
+        tf::Vector3 ang_vel_leg_pivot_frame_wrt_body(
             curr_vehicle_angular_velocity.x(),
-            curr_vehicle_angular_velocity.y() + curr_bogie_angular_velocity.at(bpe),
+            curr_vehicle_angular_velocity.y() + curr_leg_pivot_angular_velocity.at(lpe),
             curr_vehicle_angular_velocity.y()
         );
-        double cbp = curr_bogie_positions[bpe];
-        std::cout << (bp - static_cast<int>(BogiePivotEnum::LEFT_NEAR)) << ":" << cbp << ", ";
-        tf::Vector3 trans_bogie_to_wheel_frame_wrt_body(
-            (bogie_leg_length * std::cos(cbp)) - (wheel_connection_link_length * std::sin(cbp)),
+        double clp = curr_leg_pivot_positions[lpe];
+        std::cout << (lp - static_cast<int>(LegPivotEnum::LEFT_NEAR)) << ":" << clp << ", ";
+        tf::Vector3 trans_leg_pivot_to_wheel_frame_wrt_body(
+            (leg_length * std::cos(clp)) - (wheel_connection_link_length * std::sin(clp)),
             0,
-            (bogie_leg_length * std::sin(cbp)) + (wheel_connection_link_length * std::cos(cbp))
+            (leg_length * std::sin(clp)) + (wheel_connection_link_length * std::cos(clp))
         );
-        tf::Vector3 lin_vel_wheel_wrt_body = lin_vel + curr_vehicle_angular_velocity.cross(lateral_vector_origin_bogie_pivot.at(bpe))
-                                                     + ang_vel_bogie_frame_wrt_body.cross(trans_bogie_to_wheel_frame_wrt_body);
-        cont_ang[bpe] = -std::atan(lin_vel_wheel_wrt_body.z() / lin_vel_wheel_wrt_body.x());
+        tf::Vector3 lin_vel_wheel_wrt_body = lin_vel + curr_vehicle_angular_velocity.cross(lateral_vector_origin_leg_pivot.at(lpe))
+                                                     + ang_vel_leg_pivot_frame_wrt_body.cross(trans_leg_pivot_to_wheel_frame_wrt_body);
+        cont_ang[lpe] = -std::atan(lin_vel_wheel_wrt_body.z() / lin_vel_wheel_wrt_body.x());
     }
     return true;
 }
@@ -85,32 +86,32 @@ bool SRR::TractionControlContainer::estimate_contact_angles(tf::Vector3 lin_vel,
 SRR::TractionControlContainer::TractionControlContainer(
     double _wheel_radius,
     double _max_wheel_rate,
-    double _bogie_leg_length,
+    double _leg_length,
     double _wheel_connection_link_length,
-    std::string _bogie_pivot_joint_name_left_near,
-    std::string _bogie_pivot_joint_name_right_near,
-    std::string _bogie_pivot_joint_name_left_far,
-    std::string _bogie_pivot_joint_name_right_far,
-    double _lateral_distance_origin_bogie_pivot_left_near,
-    double _lateral_distance_origin_bogie_pivot_right_near,
-    double _lateral_distance_origin_bogie_pivot_left_far,
-    double _lateral_distance_origin_bogie_pivot_right_far
+    std::string _leg_pivot_joint_name_left_near,
+    std::string _leg_pivot_joint_name_right_near,
+    std::string _leg_pivot_joint_name_left_far,
+    std::string _leg_pivot_joint_name_right_far,
+    double _lateral_distance_origin_leg_pivot_left_near,
+    double _lateral_distance_origin_leg_pivot_right_near,
+    double _lateral_distance_origin_leg_pivot_left_far,
+    double _lateral_distance_origin_leg_pivot_right_far
 ) :
     wheel_radius(_wheel_radius),
     max_wheel_rate(_max_wheel_rate),
-    bogie_leg_length(_bogie_leg_length),
+    leg_length(_leg_length),
     wheel_connection_link_length(_wheel_connection_link_length),
-    bogie_pivot_joint_names({
-        {_bogie_pivot_joint_name_left_near,  LEFT_NEAR},
-        {_bogie_pivot_joint_name_right_near, RIGHT_NEAR},
-        {_bogie_pivot_joint_name_left_far,   LEFT_FAR},
-        {_bogie_pivot_joint_name_right_far,  RIGHT_FAR}
+    leg_pivot_joint_names({
+        {_leg_pivot_joint_name_left_near,  LEFT_NEAR},
+        {_leg_pivot_joint_name_right_near, RIGHT_NEAR},
+        {_leg_pivot_joint_name_left_far,   LEFT_FAR},
+        {_leg_pivot_joint_name_right_far,  RIGHT_FAR}
     }),
-    lateral_vector_origin_bogie_pivot({
-        {LEFT_NEAR,  tf::Vector3(0, _lateral_distance_origin_bogie_pivot_left_near,  0)},
-        {RIGHT_NEAR, tf::Vector3(0, _lateral_distance_origin_bogie_pivot_right_near, 0)},
-        {LEFT_FAR,   tf::Vector3(0, _lateral_distance_origin_bogie_pivot_left_far,   0)},
-        {RIGHT_FAR,  tf::Vector3(0, _lateral_distance_origin_bogie_pivot_right_far,  0)}
+    lateral_vector_origin_leg_pivot({
+        {LEFT_NEAR,  tf::Vector3(0, _lateral_distance_origin_leg_pivot_left_near,  0)},
+        {RIGHT_NEAR, tf::Vector3(0, _lateral_distance_origin_leg_pivot_right_near, 0)},
+        {LEFT_FAR,   tf::Vector3(0, _lateral_distance_origin_leg_pivot_left_far,   0)},
+        {RIGHT_FAR,  tf::Vector3(0, _lateral_distance_origin_leg_pivot_right_far,  0)}
     })
 {
     curr_dist_between_ICC_origin = 0.5;
@@ -121,16 +122,16 @@ void SRR::TractionControlContainer::handle_joint_state_callback(const sensor_msg
     for (std::size_t i = 0; i < msg.name.size(); i++)
     {
         const std::string name = msg.name[i];
-        if (bogie_pivot_joint_names.find(name) != bogie_pivot_joint_names.end())
+        if (leg_pivot_joint_names.find(name) != leg_pivot_joint_names.end())
         {
-            BogiePivotEnum bogie_pivot               = bogie_pivot_joint_names.at(name);
-            curr_bogie_positions[bogie_pivot]        = msg.position[i];
-            curr_bogie_angular_velocity[bogie_pivot] = msg.velocity[i];
+            LegPivotEnum lp                     = leg_pivot_joint_names.at(name);
+            curr_leg_pivot_positions[lp]        = msg.position[i];
+            curr_leg_pivot_angular_velocity[lp] = msg.velocity[i];
         }
     }
 
-    double bpln = curr_bogie_positions[BogiePivotEnum::LEFT_NEAR];
-    curr_longitudinal_distance_origin_axle = (bogie_leg_length * std::cos(bpln)) - (wheel_connection_link_length * std::sin(bpln));
+    double lpln = curr_leg_pivot_positions[LegPivotEnum::LEFT_NEAR];
+    curr_longitudinal_distance_origin_axle = (leg_length * std::cos(lpln)) - (wheel_connection_link_length * std::sin(lpln));
 }
 
 void SRR::TractionControlContainer::handle_imu_callback(const sensor_msgs::Imu& msg)
@@ -144,8 +145,7 @@ void SRR::TractionControlContainer::handle_imu_callback(const sensor_msgs::Imu& 
     if (std::abs(msg.angular_velocity.z) > ANG_VEL_EPS)
     {
         std::cout << "Wz:" << msg.angular_velocity.z;
-        //curr_move_type = (msg.angular_velocity.z > 0) ? MoveTypeEnum::TURN_LEFT : MoveTypeEnum::TURN_RIGHT;
-        curr_move_type = MoveTypeEnum::STRAIGHT;
+        curr_move_type = (msg.angular_velocity.z > 0) ? MoveTypeEnum::TURN_LEFT : MoveTypeEnum::TURN_RIGHT;
     }
     else
     {
@@ -165,7 +165,7 @@ void SRR::TractionControlContainer::handle_imu_callback(const sensor_msgs::Imu& 
     }
 }
 
-bool SRR::TractionControlContainer::publish_wheel_rates(BogieAbstractMap<double>& contact_angles)
+bool SRR::TractionControlContainer::publish_wheel_rates(LegAbstractMap<double>& contact_angles)
 {
     tf::Vector3 vehicle_linear_velocity;
     if (!calculate_vehicle_linear_velocity(vehicle_linear_velocity))
